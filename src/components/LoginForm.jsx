cafbb29a1292f2376584176ef241c4d1615684cd
Kamil Starmach <kstarmach@outlook.com>
2023-08-16 14:41:05 +0200

    style: Login form logo icons

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,11 +7,29 @@ const LoginForm = () => {
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
+                    <div className="flex">
+                        <div className="grow">
+                            <img
+                                className="mx-auto h-16 w-auto grow"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/AniList_logo.svg/512px-AniList_logo.svg.png"
+                                alt="Your Company"
+                            />
+                        </div>
+                        <div className="grow-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-auto h-16 stroke-2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+                            </svg>
+
+                        </div>
+                        <div className="grow mx-auto h-16 w-auto grow">
+
+                            <img
+                                className="mx-auto h-16 w-auto "
+                                src="https://upload.wikimedia.org/wikipedia/commons/7/7a/MyAnimeList_Logo.png"
+                                alt="Your Company"
+                            />
+                        </div>
+                    </div>
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Select your account
                     </h2>
