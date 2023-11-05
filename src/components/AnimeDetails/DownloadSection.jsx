ab5244a1293138519a17812cb716e96b8a921c25
kstarmach <kstarmach@outlook.com>
2023-11-05 13:59:33 +0100

    style: download table

--- src/components/AnimeDetails/DownloadSection.jsx
+++ src/components/AnimeDetails/DownloadSection.jsx
@@ -1,5 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline'
+import { ArrowDownTrayIcon, DocumentArrowDownIcon, InboxArrowDownIcon, CheckCircleIcon, ArrowDownCircleIcon, ArrowUpCircleIcon } from '@heroicons/react/24/solid'
+
+function formatDateTime(inputDateString) {
+    // Step 1: Parse the input date string into a Date object
+    const inputDate = new Date(inputDateString);
+
+    // Step 2: Format the Date object into the desired output format
+    const year = inputDate.getFullYear();
+    const month = String(inputDate.getMonth() + 1).padStart(2, '0');
+    const day = String(inputDate.getDate()).padStart(2, '0');
+    const hours = String(inputDate.getHours()).padStart(2, '0');
+    const minutes = String(inputDate.getMinutes()).padStart(2, '0');
+
+    // Create the desired output format
+    const outputDateString = `${day}-${month}-${year} ${hours}:${minutes}`;
+
+    return outputDateString;
+}
+
+
 const DownloadSection = ({ title }) => {
     const providers = ['Erai-raws', 'SubsPlease']
     const [selectedProvider, setSelectedProvider] = useState(providers[0]);
@@ -80,13 +101,13 @@ const DownloadSection = ({ title }) => {
                         <tr className="bg-gray-100 rounded-md">
                             {/* <th className="py-2 px-4 text-left">Episode</th> */}
                             <th className="py-2 px-4 text-left">Title</th>
-                            <th className="py-2 px-4 text-left">Comments Count</th>
+                            <th className="py-2 px-4 text-left"></th>
                             <th className="py-2 px-4 text-left">Links</th>
                             <th className="py-2 px-4 text-left">Size</th>
                             <th className="py-2 px-4 text-left">Date</th>
-                            <th className="py-2 px-4 text-left">S</th>
-                            <th className="py-2 px-4 text-left">L</th>
-                            <th className="py-2 px-4 text-left">D</th>
+                            <th className="py-2 px-4 text-center"><ArrowUpCircleIcon className="h-6 w-6" /></th>
+                            <th className="py-2 px-4 text-center"><ArrowDownCircleIcon className="h-6 w-6" /></th>
+                            <th className="py-2 px-4 text-center"><CheckCircleIcon className="h-6 w-6" /></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -94,25 +115,33 @@ const DownloadSection = ({ title }) => {
                             rssData.map((episode, idx) => (
                                 <tr key={idx}>
                                     {/* <td className="py-2 px-4">{episode.episodeNumber}</td> */}
-                                    <td className="py-2 px-4">
+                                    <td className="py-2 px-4 text-left">
                                         <a href={episode.guid[0]._} target="_blank" rel="noopener noreferrer">
                                             {episode.title[0]}
                                         </a>
                                     </td>
-                                    <td className="py-2 px-4">{episode['nyaa:comments'][0]}</td>
-                                    <td className="py-2 px-4 flex gap-2">
+                                    <td className="py-2 px-4 text-center">
+                                        {episode['nyaa:comments'][0] > 0 ? (
+                                            <div className="flex">
+                                                <ChatBubbleLeftRightIcon className="h-5 w-5" />
+                                                <p className="ml-1">{episode['nyaa:comments'][0]}</p>
+                                            </div>
+                                        ) : null}
+                                    </td>
+
+                                    <td className="py-2 px-4 flex gap-2 text-center">
                                         <div>
-                                            <a href={`magnet:?xt=urn:btih:${episode['nyaa:infoHash']}`} className="text-blue-500 hover:underline">M</a>
+                                            <a href={`magnet:?xt=urn:btih:${episode['nyaa:infoHash']}`} className="text-blue-500 hover:underline">   <InboxArrowDownIcon className="h-5 w-5" /></a>
                                         </div>
                                         <div>
-                                            <a href={episode.link[0]} className="text-blue-500 hover:underline">D</a>
+                                            <a href={episode.link[0]} className="text-blue-500 hover:underline"><DocumentArrowDownIcon className='h-5 w-5' /></a>
                                         </div>
                                     </td>
-                                    <td className="py-2 px-4">{episode['nyaa:size'][0]}</td>
-                                    <td className="py-2 px-4">{episode.pubDate}</td>
-                                    <td className="py-2 px-4">{episode['nyaa:seeders'][0]}</td>
-                                    <td className="py-2 px-4">{episode['nyaa:leechers'][0]}</td>
-                                    <td className="py-2 px-4">{episode['nyaa:downloads'][0]}</td>
+                                    <td className="py-2 px-4 text-center">{episode['nyaa:size'][0]}</td>
+                                    <td className="py-2 px-4 text-left">{formatDateTime(episode.pubDate)}</td>
+                                    <td className="py-2 px-4 text-green-500 text-center">{episode['nyaa:seeders'][0]}</td>
+                                    <td className="py-2 px-4 text-red-500 text-center">{episode['nyaa:leechers'][0]}</td>
+                                    <td className="py-2 px-4 text-center">{episode['nyaa:downloads'][0]}</td>
                                 </tr>
                             ))
                         ) : (
