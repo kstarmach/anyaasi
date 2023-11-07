fba3b8ac7fc1c8f12e6a78b9e1931ee7e60d2282
kstarmach <kstarmach@outlook.com>
2023-11-07 13:59:51 +0100

    feat: Multiple data sorting

--- src/components/AnimeDetails/Table.jsx
+++ src/components/AnimeDetails/Table.jsx
@@ -3,12 +3,15 @@ import axios from 'axios';
 import TableHeader from './TableHeader';
 import TableBody from './TableBody';
 import ProvidersTabs from './ProvidersTabs';
+import { CheckCircleIcon, ArrowDownCircleIcon, ArrowUpCircleIcon } from '@heroicons/react/24/solid'
+
 
 
 const Table = ({ title }) => {
     const [selectedProvider, setSelectedProvider] = useState('Erai-raws');
     const [rssData, setRssData] = useState([]);
     const [sortDirection, setSortDirection] = useState('asc');
+    const [sortColumn, setSortColumn] = useState(null); // Track the currently sorted column
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage, setItemsPerPage] = useState(10);
 
@@ -17,15 +20,27 @@ const Table = ({ title }) => {
             try {
                 const response = await axios.get(`/fetchRss/${selectedProvider}?q=${title}+1080p`);
                 if (response.status === 200) {
-                    const sortedData = [...response.data]; // Copy data for sorting
+                    // Sort the data based on the selected sorting column and direction
+                    let sortedData = [...response.data];
+                    if (sortColumn) {
+                        console.log(sortColumn);
 
-                    // Handle sorting based on the number of downloads
-                    sortedData.sort((a, b) => {
-                        const aDownloads = parseInt(a['nyaa:downloads'][0], 10);
-                        const bDownloads = parseInt(b['nyaa:downloads'][0], 10);
+                        sortedData = sortedData.sort((a, b) => {
+                            const aDownloads = parseInt(a[sortColumn][0], 10);
+                            const bDownloads = parseInt(b[sortColumn][0], 10);
 
-                        return sortDirection === 'asc' ? aDownloads - bDownloads : bDownloads - aDownloads;
-                    });
+                            return sortDirection === 'asc' ? aDownloads - bDownloads : bDownloads - aDownloads;
+                        });
+
+                        // sortedData = sortedData.sort((a, b) => {
+                        //     const aValue = a[sortColumn];
+                        //     const bValue = b[sortColumn];
+                        //     if (aValue && bValue) {
+                        //         return sortDirection === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+                        //     }
+                        //     return 0; // Leave the order unchanged if either value is undefined
+                        // });
+                    }
 
                     setRssData(sortedData);
                 }
@@ -35,12 +50,26 @@ const Table = ({ title }) => {
         };
 
         fetchData();
-    }, [selectedProvider, title, sortDirection]);
+    }, [selectedProvider, title, sortDirection, sortColumn]);
 
-    const handleSort = () => {
+    const handleSort = (columnKey) => {
         // Toggle the sort direction when the user clicks the sorting button
-        setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+        setSortDirection((prevSortDirection) => (prevSortDirection === 'asc' ? 'desc' : 'asc'));
+        setSortColumn(columnKey);
     };
+
+
+    const columns = [
+        { key: 'title', label: 'Title', sortable: false },
+        { key: 'comments', label: '', sortable: false },
+        { key: 'links', label: 'Links', sortable: false },
+        { key: 'nyaa:size', label: 'Size', sortable: true },
+        { key: 'pubDate', label: 'Date', sortable: true },
+        { key: 'nyaa:seeders', label: <ArrowUpCircleIcon className="h-6 w-6" />, sortable: true },
+        { key: 'nyaa:leechers', label: <ArrowDownCircleIcon className="h-6 w-6" />, sortable: true },
+        { key: 'nyaa:downloads', label: <CheckCircleIcon className="h-6 w-6" />, sortable: true }
+
+    ];
 
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
@@ -57,7 +86,11 @@ const Table = ({ title }) => {
             />
             <div className="bg-white shadow-sm rounded-r-lg rounded-b-lg p-8 ">
                 <table className="w-full border-collapse table-auto">
-                    <TableHeader onSort={handleSort} sortDirection={sortDirection} />
+                    <TableHeader
+                        columns={columns}
+                        onSort={handleSort}
+                        sortDirection={sortDirection}
+                    />
                     <TableBody rssData={paginatedData} />
                 </table>
             </div>
--- src/components/AnimeDetails/TableHeader.jsx
+++ src/components/AnimeDetails/TableHeader.jsx
@@ -1,67 +1,25 @@
 import { CheckCircleIcon, ArrowDownCircleIcon, ArrowUpCircleIcon } from '@heroicons/react/24/solid'
 
-const TableHeader = ({ onSort, sortDirection }) => {
+const TableHeader = ({ columns, onSort, sortDirection }) => {
     return (
         <thead>
-            <tr className="bg-gray-100 rounded-md">
-                {/* <th className="py-2 px-4 text-left">Episode</th> */}
-                <th className="py-2 px-4 text-left">
-                <div className="flex items-center">
-                        <span>Title</span>
-                        <button onClick={onSort}  className="ml-2">
-                            {sortDirection === 'asc' ? '▲' : '▼'}
-                        </button>
-                    </div>
-                </th>
-                <th className="py-2 px-4 text-left"></th>
-                <th className="py-2 px-4 text-left">Links</th>
-                <th className="py-2 px-4 text-left">
-                    <div className="flex items-center">
-                        <span>Size</span>
-                        <button onClick={onSort} className="ml-2">
-                            {sortDirection === 'asc' ? '▲' : '▼'}
-                        </button>
-                    </div>
+            <tr className="bg-gray-100 rounded-md ">
+                {columns.map((column,idx) => (
+                    <th key={column.key} className="py-2 px-4 text-left">
+                        <div className="flex items-center">
+                            <span>{column.label}</span>
+                            {column.sortable && (
+                                <button onClick={() => onSort(column.key)} className='ml-2' id={idx}>
+                                    {sortDirection === 'asc' ? '▲' : '▼'}
+                                </button>
+                            )}
+                        </div>
                     </th>
-                <th className="py-2 px-4 text-left ">
-                    <div className="flex items-center">
-                        <span>Date</span>
-                        <button onClick={onSort} className="ml-2">
-                            {sortDirection === 'asc' ? '▲' : '▼'}
-                        </button>
-                    </div>
-                </th>
-
-                <th className="py-2 px-4 text-center">
-                    <div className="flex items-center">
-                        <ArrowUpCircleIcon className="h-6 w-6" />
-                        <button onClick={onSort} className="ml-2">
-                            {sortDirection === 'asc' ? '▲' : '▼'}
-                        </button>
-                    </div>
-                </th>
-
-                <th className="py-2 px-4 text-center">
-                    <div className="flex items-center">
-                        <ArrowDownCircleIcon className="h-6 w-6" />
-                        <button onClick={onSort} className="ml-2">
-                            {sortDirection === 'asc' ? '▲' : '▼'}
-                        </button>
-                    </div>
-                </th>
-
-                <th className="py-2 px-4 text-center">
-                    <div className="flex items-center">
-                        <CheckCircleIcon className="h-6 w-6" />
-                        <button onClick={onSort} className="ml-2">
-                            {sortDirection === 'asc' ? '▲' : '▼'}
-                        </button>
-                    </div>
-                </th>
-
+                ))}
             </tr>
         </thead>
     )
 }
 
 export default TableHeader;
+
