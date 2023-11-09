c1eef630ca81c8f9a022fdb306fa17ee23122b9d
Kamil Starmach <kstarmach@outlook.com>
2023-11-09 19:28:46 +0100

    fix: Carousel scrolling

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -3,43 +3,9 @@ import Card from '../Card';
 
 
 const Carousel = ({ data, title, height, width, carouselType }) => {
-    const itemWidth = useRef(0); // Reference to store the width of a single item.
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const carousel = useRef(null);
-
-    const movePrev = () => {
-        if (currentIndex === (data.length - 1)) {
-            setCurrentIndex(data.length - 6)
-        } else {
-            setCurrentIndex((prevState) => (prevState - 1 + data.length) % data.length);
-        }
-    };
-
-    const moveNext = () => {
-        if (currentIndex === (data.length - 5)) {
-            setCurrentIndex(0)
-        } else {
-            setCurrentIndex((prevState) => (prevState + 1) % data.length);
-        }
-
-    };
-
-    useEffect(() => {
-        if (carousel.current !== null) {
-            itemWidth.current = carousel.current.scrollWidth / data.length;
-        }
-    }, [data]);
-
-
-    useEffect(() => {
-        if (carousel.current !== null) {
-            const scrollPosition = (currentIndex + data.length) % data.length * itemWidth.current;
-            carousel.current.scrollTo({
-                left: scrollPosition,
-                behavior: 'smooth'
-            });
-        }
-    }, [currentIndex, data]);
+    // We use the useRef hook to get a reference to the slider container
+    const sliderRef = useRef(null);
+    const scrollAmount = 320; // The amount to scroll when clicking the navigation buttons
 
 
     return (
@@ -48,8 +14,11 @@ const Carousel = ({ data, title, height, width, carouselType }) => {
                 <p className='text-2xl font-semibold  flex-grow pl-20'>{title}</p>
                 <div className='flex gap-4 items-center'>
                     <button
-                        onClick={movePrev}
-                        className='bg-white color-black rounded-xl p-2 flex items-center'
+                        onClick={() => {
+                            const container = sliderRef.current;
+                            container.scrollLeft -= scrollAmount; // Scroll left by the specified amount
+                        }}
+                        className='bg-white color-black rounded-xl p-2 flex items-center '
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -68,7 +37,10 @@ const Carousel = ({ data, title, height, width, carouselType }) => {
                     </button>
 
                     <button
-                        onClick={moveNext}
+                        onClick={() => {
+                            const container = sliderRef.current;
+                            container.scrollLeft += scrollAmount; // Scroll right by the specified amount
+                        }}
                         className='bg-white color-black rounded-xl p-2 flex items-center'
                     >
                         <svg
@@ -88,33 +60,32 @@ const Carousel = ({ data, title, height, width, carouselType }) => {
                     </button>
                 </div>
             </div>
-            <div className="relative overflow-hidden ">
-                <div
-                    ref={carousel}
-                    className="carousel-container relative flex gap-10 overflow-hidden  px-20 "
-                >
-                    {data.map((entry, index) => {
-                        let coverImage;
-       
-                        if (carouselType === "popular") {
-                            coverImage = entry.media.coverImage.extraLarge;
-                        } else if (carouselType === "recent") {
-                            coverImage = entry.media.bannerImage;
-                        }
 
-                        return (
-                            <Card
-                                entry={entry}
-                                key={`${entry.media.id}-${index}`}
-                                height={height}
-                                width={width}
-                                coverImage={coverImage}
-                            />
-                        );
-                    })}
-                </div>
+            {/* Image container */}
+            <div className="images-container relative flex gap-10 overflow-hidden  " ref={sliderRef}>
+                {data.map((entry, index) => {
+                    let coverImage;
+
+                    if (carouselType === "popular") {
+                        coverImage = entry.media.coverImage.extraLarge;
+                    } else if (carouselType === "recent") {
+                        coverImage = entry.media.bannerImage;
+                    }
+
+                    return (
+                        <Card
+                            entry={entry}
+                            key={`${entry.media.id}-${index}`}
+                            height={height}
+                            width={width}
+                            coverImage={coverImage}
+                        />
+                    );
+                })}
             </div>
+
         </div>
+
     );
 };
 
