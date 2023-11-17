c339bbbad2fa124d821ee90e1786ba8058ab5c42
Kamil Starmach <kstarmach@outlook.com>
2023-11-17 18:11:21 +0100

    feat: Getting user data from graphql

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,5 +1,7 @@
 const userRouter = require('express').Router();
 const axios = require('axios');
+const { ApolloClient, InMemoryCache, gql } = require('@apollo/client/core');
+//import fetch from "cross-fetch";
 
 userRouter.get('/myanimelist/:username', async (req, res) => {
 
@@ -19,8 +21,41 @@ userRouter.get('/myanimelist/:username', async (req, res) => {
     }
 })
 
-userRouter.get('/anilist/', async (req, res) => {
-    // to implement
-})
+// Import the GraphQL client setup
+const client = new ApolloClient({
+    uri: 'https://graphql.anilist.co', // Replace with your GraphQL API endpoint
+    cache: new InMemoryCache(),
+});
+
+// Define your GraphQL query
+const GET_USER_DATA = gql`
+    query ($name: String) {
+      User(name: $name) {
+        id
+        name
+        avatar {
+          large
+        }
+      }
+    }
+  `;
+
+// Define the route to fetch user data from the GraphQL API
+userRouter.get('/anilist/:username', async (req, res) => {
+    try {
+        const { username } = req.params;
+
+        // Make a request to your GraphQL API using Apollo Client
+        const { data } = await client.query({
+            query: GET_USER_DATA,
+            variables: { name: username },
+        });
+
+        res.json(data.User);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+});
 
 module.exports = userRouter;
