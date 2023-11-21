b46f0aca25e797a32baa67b2ccbda7114c06b230
Kamil Starmach <kstarmach@outlook.com>
2023-11-21 23:39:31 +0100

    style: Information icon

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import Anilist from '../components/LoginForm/Anilist';
 import MyAnimeList from '../components/LoginForm/MyAnimeList';
-
+import { ArrowsRightLeftIcon } from '@heroicons/react/24/solid'
 const Login = () => {
     return (
         <>
             <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                 Select your account
             </h2>
-            <div className='flex flex-wrap'>
+            <div className='flex flex-wrap '>
                 <div className='flex-1'></div>
                 <Anilist />
+
+                <ArrowsRightLeftIcon className="w-16 h-auto   " />
 
                 <MyAnimeList />
                 <div className='flex-1'></div>
