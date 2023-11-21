aa4995cbda0b11968bfbcad78f37ca6215b58425
Kamil Starmach <kstarmach@outlook.com>
2023-11-21 17:54:12 +0100

    fix: Unnecessary Query

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,38 +3,49 @@ import RecentCarousel from "../components/Carousel/RecentCarousel";
 import { useUserContext } from "../UserContext";
 import { useQuery } from "@apollo/client";
 import { GET_ANIME_LIST } from "../queries";
+import Login from './Login';
+
+import { useEffect } from 'react';
 
 const Home = () => {
-    const { user } = useUserContext();
+  const { user } = useUserContext();
+  const { loading, error, data, refetch } = useQuery(GET_ANIME_LIST, {
+    variables: { userId: user?.id },
+    skip: !user?.id, // Skip the query if user.id is falsy
+  });
 
-    const { loading, error, data } = useQuery(GET_ANIME_LIST, {
-        variables: { userId: user?.id },
-    });
-
-    // Check if user.id exists before rendering or using data
-    if (!user?.id) {
-        // Handle the case when user.id is not available
-        return <p>Loading...</p>;
+  // useEffect to refetch data when user.id becomes available
+  useEffect(() => {
+    if (user?.id) {
+      refetch({ userId: user.id });
     }
+  }, [user?.id, refetch]);
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  // Check if user.id exists before rendering or using data
+  if (!user?.id) {
+    // Handle the case when user.id is not available
+    return <Login />;
+  }
 
-    if (error) {
-        return <p>Error: {error.message}</p>;
-    }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-    const animeListCollection = data.MediaListCollection;
-    const animeLists = animeListCollection.lists;
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
-    return (
-        <>
-            <PopularCarousel animeLists={animeLists} />
-            <RecentCarousel animeLists={animeLists} />
-        </>
-    );
-}
+  const animeListCollection = data.MediaListCollection;
+  const animeLists = animeListCollection.lists;
+
+  return (
+    <>
+      <PopularCarousel animeLists={animeLists} />
+      <RecentCarousel animeLists={animeLists} />
+    </>
+  );
+};
+
 
 
 export default Home;
