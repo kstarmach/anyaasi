475dbbe09f29fe7d3801c75f1d360c502c46db7e
Kamil Starmach <kstarmach@outlook.com>
2023-11-24 17:09:02 +0100

    fix: Oauth2 on the same tab

--- client/src/components/LoginForm/MyAnimeList.jsx
+++ client/src/components/LoginForm/MyAnimeList.jsx
@@ -12,7 +12,7 @@ const MyAnimeListForm = ({ handleLogin, redirectUrl }) => {
                 <div>
                     <a
                         href={redirectUrl}
-                        target='blank'
+                        //target='blank'
                         className="cursor-pointer flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Login
