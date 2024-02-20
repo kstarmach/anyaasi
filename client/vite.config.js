7cbec8fb08f1359f3216c674532523928f64d95b
kstarmach <kstarmach@outlook.com>
2024-02-20 13:33:16 +0100

    Google verification

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,15 +8,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
-// const proxy = process.env.SERVER_URI;
+const proxy = process.env.SERVER_URI;
+//const proxy = 'http://localhost:3000/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/rss/': proxy, // Change this to the address of your Node server
-      // '/anilist/': proxy, // Change this to the address of your Node server
+      '/rss/': proxy, // Change this to the address of your Node server
+      '/anilist/': proxy // Change this to the address of your Node server
       // '/myanimelist/': proxy, // Change this to the address of your Node server
       // '/api': {
       //   target: 'https://api.myanimelist.net',
