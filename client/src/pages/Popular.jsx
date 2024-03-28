46121dbe1a46aa84d178473a8094e7468ee983f4
Kamil Starmach <kstarmach@outlook.com>
2024-03-28 18:06:10 +0100

    style: Popular anime grid fix

--- client/src/pages/Popular.jsx
+++ client/src/pages/Popular.jsx
@@ -13,13 +13,13 @@ const Popular = () => {
     return (
         <div className="mx-20">
             <p className='text-2xl font-semibold  flex-grow mb-10'>Popular</p>
-            <div className="flex flex-wrap justify-center gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 min-[2000px]:grid-cols-6  min-[2300px]:grid-cols-7 justify-center gap-4">
 
                 {entries.map((entry, idx) => (
                     <Card
                         entry={entry}
-                        height={550}
-                        width={450}
+                        height={400}
+                        width={300}
                         coverImage={entry.coverImage.extraLarge}
                         key={idx}
                     />
