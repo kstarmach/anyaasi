104b77089deeb8f15af743e9579883553024775a
Kamil Starmach <kstarmach@outlook.com>
2024-04-02 15:35:05 +0200

    style: User menu

--- client/src/components/Nav/UserOptions.jsx
+++ client/src/components/Nav/UserOptions.jsx
@@ -40,10 +40,10 @@ const UserOptions = () => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-[rgba(0,0,5,0.1)_10px_5px_4px_0px] focus:outline-none">
                     <div className="py-1">
                         <Menu.Item>
-                            <span className='block w-full px-4 py-2 text-left text-sm '> Signed in as <span className='text-slate-400'> {user.name}</span></span>
+                            <span className='block w-full px-4 py-2 text-left text-sm '> Signed in as <span className='text-indigo-600'> {user.name}</span></span>
                         </Menu.Item>
 
                         <hr />
