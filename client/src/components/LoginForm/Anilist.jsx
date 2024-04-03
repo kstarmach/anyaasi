eb83e54fa2d9102c7319c178bba8c59ec3a43579
Kamil Starmach <kstarmach@outlook.com>
2024-04-03 12:37:23 +0200

    fix: Delay on submit

--- client/src/components/LoginForm/Anilist.jsx
+++ client/src/components/LoginForm/Anilist.jsx
@@ -2,53 +2,48 @@ import { useState } from 'react';
 
 import UserResult from "./UserResult";
 
-const AnilistForm = ({ handleForm }) => {
+const AnilistForm = ({ handleForm, handleInputChange }) => {
     return (
-
         <form className="flex flex-col gap-4" onSubmit={handleForm}>
             <input
                 id="username"
                 name="username"
                 autoComplete='username'
-                min={3}
                 placeholder='Anilist.co username...'
                 required
                 className="flex items-center bg-white rounded-full p-3 shadow-[rgba(0,0,5,0.1)_10px_5px_4px_0px]"
+                onChange={handleInputChange}
             />
         </form>
-
-    )
-}
+    );
+};
 
 const Anilist = () => {
     const [username, setUsername] = useState('');
+    const [typingTimeout, setTypingTimeout] = useState(0);
+
     const handleForm = (event) => {
         event.preventDefault();
-        setUsername(event.target[0].value);
-    }
+        setUsername(event.target.username.value);
+    };
+
+    const handleInputChange = (event) => {
+        clearTimeout(typingTimeout);
+        const inputUsername = event.target.value;
+        setTypingTimeout(setTimeout(() => setUsername(inputUsername), 700));
+    };
 
     return (
         <div className="flex flex-col gap-10">
-
-
             <img
                 className="mx-auto h-20 w-auto grow rounded-xl shadow-[rgba(0,0,5,0.1)_10px_5px_4px_0px]"
                 src="AniList_logo.svg.png"
                 alt="Anilist logo"
             />
-
-
-            <AnilistForm
-                setUsername={setUsername}
-                handleForm={handleForm}
-            />
-            {username ?
-                <UserResult
-                    username={username}
-                />
-                : ''}
+            <AnilistForm handleForm={handleForm} handleInputChange={handleInputChange} />
+            {username && <UserResult username={username} />}
         </div>
-    )
-}
+    );
+};
 
-export default Anilist
+export default Anilist;
