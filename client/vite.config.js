6f0c2116b87e911f09dd6f26f7fd03b2bd0b9a29
Kamil Starmach <kstarmach@outlook.com>
2024-08-16 23:04:43 +0200

    feat: Added Dockerfile and docker-compose.yml for development environment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,29 +2,21 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-
 import dotenv from 'dotenv';
 
 dotenv.config()
 
-
 // eslint-disable-next-line no-undef
-const proxy = process.env.SERVER_URI;
-//const proxy = 'http://localhost:3000/';
+const proxy = process.env.SERVER_URI || 'http://localhost:3000';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/rss/': proxy, // Change this to the address of your Node server
-      '/anilist/': proxy // Change this to the address of your Node server
-      // '/myanimelist/': proxy, // Change this to the address of your Node server
-      // '/api': {
-      //   target: 'https://api.myanimelist.net',
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ''),
-      // },
+      '/rss/': proxy,
+      '/anilist/': proxy
     },
-  },
+    host: true,
+    port: 8000
+  }
 })
